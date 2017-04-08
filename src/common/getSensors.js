ae04ec26adae171494db0cc584e81b6f8935cfe9
MKapravchuk <hameleonick@MKapravchuks-MacBook-Pro.local>
2017-04-08 14:40:03 +0200

    only for test

--- src/common/getSensors.js
+++ src/common/getSensors.js
@@ -3,10 +3,10 @@ import fetch from 'node-fetch';
 const BASE = 'https://airapi.airly.eu/v1/sensors/current';
 
 export default function getSensors(latMin, lonMin, latMax, lonMax, { apiKey }) {
-  let southwestLat =  Math.max(latMin, latMax);
-  let southwestLong =  Math.max(lonMin, lonMax);
-  let northeastLat = Math.min(latMin, latMax);
-  let northeastLong = Math.min(lonMin, lonMax);
+  let southwestLat =  Math.max(latMin, latMax) + 0.015;
+  let southwestLong =  Math.max(lonMin, lonMax) + 0.015;
+  let northeastLat = Math.min(latMin, latMax) + 0.015;
+  let northeastLong = Math.min(lonMin, lonMax) + 0.015;
 
   const url = `${BASE}?southwestLat=${southwestLat}&southwestLong=${southwestLong}&northeastLat=${northeastLat}&northeastLong=${northeastLong}`;
 
