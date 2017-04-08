a1fe6479184624b9450e4e61046d6380ddfbd369
Daniel Słaby <pufek93@gmail.com>
2017-04-08 12:36:52 +0200

    eqwewqe

--- src/modules/map.js
+++ src/modules/map.js
@@ -2,6 +2,8 @@ import {createAction, handleActions} from 'redux-actions'
 import _ from 'lodash'
 export const SETTING_A_POINT = 'SETTING_A_POINT'
 export const SETTING_B_POINT = 'SETTING_B_POINT'
+export const SET_POINT_A = 'SET_POINT_A'
+export const SET_POINT_B = 'SET_POINT_B'
 
 const defaultState = {
     pointA: null,
@@ -23,7 +25,26 @@ const reducer = (state = defaultState, action) => {
                 isSettingPointBEnabled: true
             })
             break;
+        case SET_POINT_A:
+            return Object.assign(clonedObject, {
+                isSettingPointAEnabled: false,
+                pointA: {
+                    lat: 1,// use data from action
+                    lng: 2 // use data from action
+                }
+            })
+            break;
+        case SET_POINT_B:
+            return Object.assign(clonedObject, {
+                isSettingPointBEnabled: false,
+                pointB: {
+                    lat: 1,// use data from action
+                    lng: 2 // use data from action
+                }
+            })
+            break;
     }
     return defaultState;
 }
+
 export default  reducer;
