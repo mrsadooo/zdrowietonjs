893c9a21a9c8662c76e7e27efefd703016ced799
Piotr Sadowski <sadooo86@gmail.com>
2017-04-08 12:52:18 +0200

    Add calculatePath function

---
+++ src/common/calculatePath.js
@@ -0,0 +1,20 @@
+import distance from './distance';
+import { AStar } from 'path-finder';
+import Sensor from './Sensor';
+import neighbours from './neighbours';
+
+export default function(start, end, data) {
+  const sensors = data.map(toSensor);
+  const getNeighbours = neighbours(start, end, sensors);
+
+  const aStar = new AStar({
+    metric: distance,
+    neighbours: getNeighbours
+  });
+
+  return aStar.find(start, end);
+}
+
+function toSensor({ location, id, pollutionLevel }) {
+  return new Sensor(location.latitude, location.longitude, pollutionLevel, id);
+}
