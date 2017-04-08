3d8f43f022b415c71289c93a64f2950673b477b8
Daniel Słaby <pufek93@gmail.com>
2017-04-08 11:49:32 +0200

    added two actions

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,8 @@
 // import combineReducers from '../store/combineReducers'; // to use with immutable state
-// import { combineReducers } from 'redux';
+import {combineReducers} from 'redux';
+import MapReducer from './../modules/map'
 
-// export default combineReducers({});
-export default state => state;
+export default combineReducers({
+    map: MapReducer
+});
+// export default state => state;
